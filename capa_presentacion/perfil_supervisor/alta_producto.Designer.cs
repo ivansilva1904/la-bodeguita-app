66eaa32883b70cf07cc47d50f3052dce88ac4703
Guillermo Chiari <chiari_guille@live.com>
2023-09-19 14:33:05 -0300

    agregado alta_producto

---
+++ capa_presentacion/perfil_supervisor/alta_producto.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace capa_presentacion.perfil_supervisor
+{
+    partial class alta_producto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlAltaProducto = new System.Windows.Forms.Panel();
+            this.btnImagenProducto = new System.Windows.Forms.Button();
+            this.btnGuardarProducto = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.cbxProveedor = new System.Windows.Forms.ComboBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtLinea = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txtIdProducto = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblLinea = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblTipoProducto = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblIdProducto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlAltaProducto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlAltaProducto
+            // 
+            this.pnlAltaProducto.BackColor = System.Drawing.Color.Beige;
+            this.pnlAltaProducto.Controls.Add(this.pictureBox1);
+            this.pnlAltaProducto.Controls.Add(this.label1);
+            this.pnlAltaProducto.Controls.Add(this.btnImagenProducto);
+            this.pnlAltaProducto.Controls.Add(this.btnGuardarProducto);
+            this.pnlAltaProducto.Controls.Add(this.txtDescripcion);
+            this.pnlAltaProducto.Controls.Add(this.txtPrecio);
+            this.pnlAltaProducto.Controls.Add(this.cbxProveedor);
+            this.pnlAltaProducto.Controls.Add(this.txtMarca);
+            this.pnlAltaProducto.Controls.Add(this.txtLinea);
+            this.pnlAltaProducto.Controls.Add(this.radioButton2);
+            this.pnlAltaProducto.Controls.Add(this.radioButton1);
+            this.pnlAltaProducto.Controls.Add(this.txtIdProducto);
+            this.pnlAltaProducto.Controls.Add(this.lblDescripcion);
+            this.pnlAltaProducto.Controls.Add(this.lblLinea);
+            this.pnlAltaProducto.Controls.Add(this.lblPrecio);
+            this.pnlAltaProducto.Controls.Add(this.lblMarca);
+            this.pnlAltaProducto.Controls.Add(this.lblTipoProducto);
+            this.pnlAltaProducto.Controls.Add(this.label2);
+            this.pnlAltaProducto.Controls.Add(this.lblIdProducto);
+            this.pnlAltaProducto.Location = new System.Drawing.Point(12, 12);
+            this.pnlAltaProducto.Name = "pnlAltaProducto";
+            this.pnlAltaProducto.Size = new System.Drawing.Size(693, 404);
+            this.pnlAltaProducto.TabIndex = 0;
+            // 
+            // btnImagenProducto
+            // 
+            this.btnImagenProducto.Location = new System.Drawing.Point(530, 306);
+            this.btnImagenProducto.Name = "btnImagenProducto";
+            this.btnImagenProducto.Size = new System.Drawing.Size(127, 27);
+            this.btnImagenProducto.TabIndex = 16;
+            this.btnImagenProducto.Text = "Agregar Imagen";
+            this.btnImagenProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarProducto
+            // 
+            this.btnGuardarProducto.Location = new System.Drawing.Point(530, 359);
+            this.btnGuardarProducto.Name = "btnGuardarProducto";
+            this.btnGuardarProducto.Size = new System.Drawing.Size(127, 27);
+            this.btnGuardarProducto.TabIndex = 15;
+            this.btnGuardarProducto.Text = "Guardar Producto";
+            this.btnGuardarProducto.UseVisualStyleBackColor = true;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(140, 269);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(285, 21);
+            this.txtDescripcion.TabIndex = 14;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(103, 217);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 21);
+            this.txtPrecio.TabIndex = 13;
+            // 
+            // cbxProveedor
+            // 
+            this.cbxProveedor.FormattingEnabled = true;
+            this.cbxProveedor.Location = new System.Drawing.Point(500, 58);
+            this.cbxProveedor.Name = "cbxProveedor";
+            this.cbxProveedor.Size = new System.Drawing.Size(130, 21);
+            this.cbxProveedor.TabIndex = 12;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(104, 163);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(100, 21);
+            this.txtMarca.TabIndex = 11;
+            // 
+            // txtLinea
+            // 
+            this.txtLinea.Location = new System.Drawing.Point(279, 163);
+            this.txtLinea.Name = "txtLinea";
+            this.txtLinea.Size = new System.Drawing.Size(100, 21);
+            this.txtLinea.TabIndex = 10;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(269, 111);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(64, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "TIPO 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(176, 111);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "TIPO 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // txtIdProducto
+            // 
+            this.txtIdProducto.Location = new System.Drawing.Point(159, 58);
+            this.txtIdProducto.Name = "txtIdProducto";
+            this.txtIdProducto.Size = new System.Drawing.Size(100, 21);
+            this.txtIdProducto.TabIndex = 7;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(55, 272);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(73, 13);
+            this.lblDescripcion.TabIndex = 6;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // lblLinea
+            // 
+            this.lblLinea.AutoSize = true;
+            this.lblLinea.Location = new System.Drawing.Point(222, 166);
+            this.lblLinea.Name = "lblLinea";
+            this.lblLinea.Size = new System.Drawing.Size(37, 13);
+            this.lblLinea.TabIndex = 5;
+            this.lblLinea.Text = "Linea";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(57, 220);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(42, 13);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(55, 166);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(41, 13);
+            this.lblMarca.TabIndex = 3;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblTipoProducto
+            // 
+            this.lblTipoProducto.AutoSize = true;
+            this.lblTipoProducto.Location = new System.Drawing.Point(55, 113);
+            this.lblTipoProducto.Name = "lblTipoProducto";
+            this.lblTipoProducto.Size = new System.Drawing.Size(85, 13);
+            this.lblTipoProducto.TabIndex = 2;
+            this.lblTipoProducto.Text = "Tipo Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cuit Proveedor";
+            // 
+            // lblIdProducto
+            // 
+            this.lblIdProducto.AutoSize = true;
+            this.lblIdProducto.Location = new System.Drawing.Point(57, 61);
+            this.lblIdProducto.Name = "lblIdProducto";
+            this.lblIdProducto.Size = new System.Drawing.Size(75, 13);
+            this.lblIdProducto.TabIndex = 0;
+            this.lblIdProducto.Text = "ID Producto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(291, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nuevo Producto";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(469, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 189);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // alta_producto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(717, 428);
+            this.Controls.Add(this.pnlAltaProducto);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "alta_producto";
+            this.Text = "alta_producto";
+            this.pnlAltaProducto.ResumeLayout(false);
+            this.pnlAltaProducto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlAltaProducto;
+        private System.Windows.Forms.Button btnImagenProducto;
+        private System.Windows.Forms.Button btnGuardarProducto;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.ComboBox cbxProveedor;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtLinea;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox txtIdProducto;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblLinea;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblTipoProducto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblIdProducto;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
