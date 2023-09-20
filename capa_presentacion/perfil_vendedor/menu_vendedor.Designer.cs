76709d32736216034ed1fdb902fdc83d6b1e8da1
ivansilva1904 <ivan.silva04599@gmail.com>
2023-09-20 09:52:24 -0300

    modificaciones al menu_vendedor

--- capa_presentacion/perfil_vendedor/menu_vendedor.Designer.cs
+++ capa_presentacion/perfil_vendedor/menu_vendedor.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAltaCliente = new System.Windows.Forms.Button();
+            this.btnModificarCliente = new System.Windows.Forms.Button();
+            this.btnRealizarVenta = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tan;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnRealizarVenta);
+            this.panel1.Controls.Add(this.btnModificarCliente);
+            this.panel1.Controls.Add(this.btnAltaCliente);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 466);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(254, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(733, 466);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnAltaCliente
+            // 
+            this.btnAltaCliente.FlatAppearance.BorderSize = 0;
+            this.btnAltaCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAltaCliente.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAltaCliente.Location = new System.Drawing.Point(-2, 123);
+            this.btnAltaCliente.Name = "btnAltaCliente";
+            this.btnAltaCliente.Size = new System.Drawing.Size(257, 50);
+            this.btnAltaCliente.TabIndex = 1;
+            this.btnAltaCliente.Text = "Alta cliente";
+            this.btnAltaCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarCliente
+            // 
+            this.btnModificarCliente.FlatAppearance.BorderSize = 0;
+            this.btnModificarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificarCliente.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificarCliente.Location = new System.Drawing.Point(-2, 179);
+            this.btnModificarCliente.Name = "btnModificarCliente";
+            this.btnModificarCliente.Size = new System.Drawing.Size(257, 50);
+            this.btnModificarCliente.TabIndex = 2;
+            this.btnModificarCliente.Text = "Modificar cliente";
+            this.btnModificarCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnRealizarVenta
+            // 
+            this.btnRealizarVenta.FlatAppearance.BorderSize = 0;
+            this.btnRealizarVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRealizarVenta.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRealizarVenta.Location = new System.Drawing.Point(-2, 235);
+            this.btnRealizarVenta.Name = "btnRealizarVenta";
+            this.btnRealizarVenta.Size = new System.Drawing.Size(257, 50);
+            this.btnRealizarVenta.TabIndex = 3;
+            this.btnRealizarVenta.Text = "Realizar venta";
+            this.btnRealizarVenta.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::capa_presentacion.Properties.Resources.logosinfondo1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 104);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menu_vendedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "menu_vendedor";
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "menu_vendedor";
+            this.Text = "La bodeguita - Menu";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRealizarVenta;
+        private System.Windows.Forms.Button btnModificarCliente;
+        private System.Windows.Forms.Button btnAltaCliente;
     }
 }
