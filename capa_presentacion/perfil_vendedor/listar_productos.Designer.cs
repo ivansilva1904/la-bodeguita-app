ed307b4a314bef7291f73557ad76dc5c6acd5c31
ivansilva1904 <ivan.silva04599@gmail.com>
2023-09-21 16:28:45 -0300

    agregado form listar_productos a vendedor

---
+++ capa_presentacion/perfil_vendedor/listar_productos.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace capa_presentacion.perfil_vendedor
+{
+    partial class listar_productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblListadoProductos = new System.Windows.Forms.Label();
+            this.dgvListadoProductos = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colProveedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipoProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMarca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLinea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colStock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvListadoProductos);
+            this.panel1.Controls.Add(this.lblListadoProductos);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 442);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblListadoProductos
+            // 
+            this.lblListadoProductos.AutoSize = true;
+            this.lblListadoProductos.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListadoProductos.Location = new System.Drawing.Point(223, 19);
+            this.lblListadoProductos.Name = "lblListadoProductos";
+            this.lblListadoProductos.Size = new System.Drawing.Size(265, 16);
+            this.lblListadoProductos.TabIndex = 0;
+            this.lblListadoProductos.Text = "Listado de los productos registados";
+            // 
+            // dgvListadoProductos
+            // 
+            this.dgvListadoProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colDescripcion,
+            this.colProveedor,
+            this.colTipoProducto,
+            this.colMarca,
+            this.colLinea,
+            this.colPrecio,
+            this.colStock});
+            this.dgvListadoProductos.Location = new System.Drawing.Point(33, 64);
+            this.dgvListadoProductos.Name = "dgvListadoProductos";
+            this.dgvListadoProductos.Size = new System.Drawing.Size(642, 337);
+            this.dgvListadoProductos.TabIndex = 1;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID Producto";
+            this.colID.Name = "colID";
+            this.colID.ReadOnly = true;
+            // 
+            // colDescripcion
+            // 
+            this.colDescripcion.HeaderText = "Descripcion";
+            this.colDescripcion.Name = "colDescripcion";
+            this.colDescripcion.ReadOnly = true;
+            // 
+            // colProveedor
+            // 
+            this.colProveedor.HeaderText = "Proveedor";
+            this.colProveedor.Name = "colProveedor";
+            this.colProveedor.ReadOnly = true;
+            // 
+            // colTipoProducto
+            // 
+            this.colTipoProducto.HeaderText = "Tipo producto";
+            this.colTipoProducto.Name = "colTipoProducto";
+            this.colTipoProducto.ReadOnly = true;
+            // 
+            // colMarca
+            // 
+            this.colMarca.HeaderText = "Marca";
+            this.colMarca.Name = "colMarca";
+            this.colMarca.ReadOnly = true;
+            // 
+            // colLinea
+            // 
+            this.colLinea.HeaderText = "Linea";
+            this.colLinea.Name = "colLinea";
+            this.colLinea.ReadOnly = true;
+            // 
+            // colPrecio
+            // 
+            this.colPrecio.HeaderText = "Precio";
+            this.colPrecio.Name = "colPrecio";
+            this.colPrecio.ReadOnly = true;
+            // 
+            // colStock
+            // 
+            this.colStock.HeaderText = "Stock";
+            this.colStock.Name = "colStock";
+            this.colStock.ReadOnly = true;
+            // 
+            // listar_productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(733, 466);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "listar_productos";
+            this.Text = "listar_productos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblListadoProductos;
+        private System.Windows.Forms.DataGridView dgvListadoProductos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProveedor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipoProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMarca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLinea;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrecio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStock;
+    }
+}
