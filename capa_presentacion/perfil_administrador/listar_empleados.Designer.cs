ba0426cfff380d2ef03db0e6a169037cf87ef99c
Guillermo Chiari <chiari_guille@live.com>
2023-09-29 20:34:43 -0300

    agregado listar_empleados

---
+++ capa_presentacion/perfil_administrador/listar_empleados.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace capa_presentacion.perfil_administrador
+{
+    partial class listar_empleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlListarEmpleados = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.dniEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaNac = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaIncorporacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.activo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnlListarEmpleados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlListarEmpleados
+            // 
+            this.pnlListarEmpleados.BackColor = System.Drawing.Color.Beige;
+            this.pnlListarEmpleados.Controls.Add(this.lblDni);
+            this.pnlListarEmpleados.Controls.Add(this.button1);
+            this.pnlListarEmpleados.Controls.Add(this.textBox1);
+            this.pnlListarEmpleados.Controls.Add(this.dataGridView1);
+            this.pnlListarEmpleados.Controls.Add(this.label1);
+            this.pnlListarEmpleados.Location = new System.Drawing.Point(12, 12);
+            this.pnlListarEmpleados.Name = "pnlListarEmpleados";
+            this.pnlListarEmpleados.Size = new System.Drawing.Size(709, 442);
+            this.pnlListarEmpleados.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(291, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista Empleados";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dniEmpleado,
+            this.nombre,
+            this.apellido,
+            this.telefono,
+            this.fechaNac,
+            this.fechaIncorporacion,
+            this.activo,
+            this.tipo});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(703, 336);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(14, 79);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(29, 13);
+            this.lblDni.TabIndex = 4;
+            this.lblDni.Text = "DNI";
+            // 
+            // dniEmpleado
+            // 
+            this.dniEmpleado.HeaderText = "DNI";
+            this.dniEmpleado.Name = "dniEmpleado";
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.Name = "apellido";
+            // 
+            // telefono
+            // 
+            this.telefono.HeaderText = "Telefono";
+            this.telefono.Name = "telefono";
+            // 
+            // fechaNac
+            // 
+            this.fechaNac.HeaderText = "Fecha Nacimiento";
+            this.fechaNac.Name = "fechaNac";
+            // 
+            // fechaIncorporacion
+            // 
+            this.fechaIncorporacion.HeaderText = "Fecha Incorporacion";
+            this.fechaIncorporacion.Name = "fechaIncorporacion";
+            // 
+            // activo
+            // 
+            this.activo.HeaderText = "Activo";
+            this.activo.Name = "activo";
+            // 
+            // tipo
+            // 
+            this.tipo.HeaderText = "Tipo Empleado";
+            this.tipo.Name = "tipo";
+            // 
+            // listar_empleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::capa_presentacion.Properties.Resources.pexels_photo_15455291;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(733, 466);
+            this.Controls.Add(this.pnlListarEmpleados);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "listar_empleados";
+            this.Text = "listar_empleados";
+            this.pnlListarEmpleados.ResumeLayout(false);
+            this.pnlListarEmpleados.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlListarEmpleados;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dniEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaNac;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaIncorporacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn activo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ capa_presentacion/perfil_administrador/listar_empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capa_presentacion.perfil_administrador
+{
+    public partial class listar_empleados : Form
+    {
+        public listar_empleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
