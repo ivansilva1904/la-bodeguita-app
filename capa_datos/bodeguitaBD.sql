b6fedeb531873f9d974838de926d7b5fe710328c
ivansilva1904 <ivan.silva04599@gmail.com>
2023-10-11 10:54:37 -0300

    agregada tabla empleados y tipoEmpleados a script sql

--- capa_datos/bodeguitaBD.sql
+++ capa_datos/bodeguitaBD.sql
@@ -3,6 +3,27 @@ CREATE DATABASE bodeguitaBD;
 
 USE bodeguitaBD;
 
+CREATE TABLE tiposEmpleados(
+	idTipoEmpleado INT IDENTITY PRIMARY KEY,
+	descripcion VARCHAR(30)
+);
+
+CREATE TABLE empleados(
+	dniEmpleado INT PRIMARY KEY,
+	nombre VARCHAR(30),
+	apellido VARCHAR(30),
+	fechaNac DATE,
+	fechaIncorp DATE,
+	fechaDeshab DATE,
+	direccion VARCHAR(30),
+	telefono VARCHAR(30),
+	email VARCHAR(30),
+	contraseña VARCHAR(30),
+	idTipoEmpleado INT,
+	baja BIT,
+	CONSTRAINT fk_tipoEmpleado FOREIGN KEY (idTipoEmpleado) REFERENCES tiposEmpleados(idTipoEmpleado)
+);
+
 CREATE TABLE clientes(
 	dniCliente INT PRIMARY KEY,
 	nombre VARCHAR(30),
