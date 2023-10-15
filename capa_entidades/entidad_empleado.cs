5b2f61b2d69aedf859eb56cadcd5e660265dc055
ivansilva1904 <ivan.silva04599@gmail.com>
2023-10-15 19:27:27 -0300

    codigo de entidad_empleado.cs terminado

--- capa_entidades/entidad_empleado.cs
+++ capa_entidades/entidad_empleado.cs
@@ -8,6 +8,30 @@ namespace capa_entidades
 {
     public class EntidadEmpleado
     {
+        private int dniEmpleado;
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNac;
+        private DateTime fechaIncorp;
+        private DateTime fechaDeshab;
+        private string direccion;
+        private string telefono;
+        private string email;
+        private string contraseña;
+        private int idTipoEmpleado;
+        private bool baja;
 
+        public int DniEmpleado { get => dniEmpleado; set => dniEmpleado = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+        public DateTime FechaIncorp { get => fechaIncorp; set => fechaIncorp = value; }
+        public DateTime FechaDeshab { get => fechaDeshab; set => fechaDeshab = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Email { get => email; set => email = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        public int IdTipoEmpleado { get => idTipoEmpleado; set => idTipoEmpleado = value; }
+        public bool Baja { get => baja; set => baja = value; }
     }
 }
