053f3b917f257366f16eb99273e8881ca52f6ba8
Guillermo Chiari <chiari_guille@live.com>
2023-10-23 13:35:01 -0300

    modificado bodeguitaBD sql

--- capa_datos/bodeguitaBD.sql
+++ capa_datos/bodeguitaBD.sql
@@ -38,3 +38,24 @@ CREATE TABLE clientes(
 INSERT INTO tiposEmpleados(descripcion) VALUES('Vendedor');
 INSERT INTO tiposEmpleados(descripcion) VALUES('Supervisor');
 INSERT INTO tiposEmpleados(descripcion) VALUES('Administrador');
+
+/* Agregada parte de proveedor 23/10/ */
+
+CREATE TABLE proveedor(
+	cuitProveedor BIGINT PRIMARY KEY,
+	razonSocial VARCHAR(50),
+	direccion VARCHAR(30),
+	telefono VARCHAR(30),
+	email VARCHAR(30),
+	fechaAlta DATE,
+	fechaBaja DATE,
+	baja BIT DEFAULT 0
+);
+
+/* datos de prueba sin DATE*/
+
+INSERT INTO proveedor(cuitProveedor,razonSocial,direccion,telefono,email) VALUES(77111111117,'Whiskeria UNO','Calle Falsa 111','3794000111','whiskeriaUNO@gmail.com');
+INSERT INTO proveedor(cuitProveedor,razonSocial,direccion,telefono,email) VALUES(77222222227,'CERVECERIA DOS','Calle Falsa 222','3794000222','cerveceriaDOS@live.com');
+INSERT INTO proveedor(cuitProveedor,razonSocial,direccion,telefono,email) VALUES(77333333337,'Viñedo tres','Calle Falsa 333','3794000333','viñedosTRES@gmail.com');
+
+/**/
