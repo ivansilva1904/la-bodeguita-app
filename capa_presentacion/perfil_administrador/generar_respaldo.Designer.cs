b379076290d9b14118fcc4b6f3318c84b3a93925
ivansilva1904 <ivan.silva04599@gmail.com>
2023-11-08 19:05:18 -0300

    cambios visuales en form generar_respaldo

--- capa_presentacion/perfil_administrador/generar_respaldo.Designer.cs
+++ capa_presentacion/perfil_administrador/generar_respaldo.Designer.cs
@@ -33,6 +33,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtNombreRespaldo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblDirectorio = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,6 +45,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Beige;
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lblDirectorio);
             this.panel1.Controls.Add(this.btnGenerarRespaldo);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.txtNombreRespaldo);
@@ -55,9 +61,9 @@
             // btnGenerarRespaldo
             // 
             this.btnGenerarRespaldo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGenerarRespaldo.Location = new System.Drawing.Point(507, 98);
+            this.btnGenerarRespaldo.Location = new System.Drawing.Point(282, 318);
             this.btnGenerarRespaldo.Name = "btnGenerarRespaldo";
-            this.btnGenerarRespaldo.Size = new System.Drawing.Size(129, 21);
+            this.btnGenerarRespaldo.Size = new System.Drawing.Size(129, 30);
             this.btnGenerarRespaldo.TabIndex = 3;
             this.btnGenerarRespaldo.Text = "Generar Respaldo";
             this.btnGenerarRespaldo.UseVisualStyleBackColor = true;
@@ -66,7 +72,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 101);
+            this.label2.Location = new System.Drawing.Point(67, 218);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 13);
             this.label2.TabIndex = 2;
@@ -74,7 +80,7 @@
             // 
             // txtNombreRespaldo
             // 
-            this.txtNombreRespaldo.Location = new System.Drawing.Point(165, 98);
+            this.txtNombreRespaldo.Location = new System.Drawing.Point(199, 215);
             this.txtNombreRespaldo.Name = "txtNombreRespaldo";
             this.txtNombreRespaldo.Size = new System.Drawing.Size(326, 21);
             this.txtNombreRespaldo.TabIndex = 1;
@@ -89,6 +95,32 @@
             this.label1.Size = new System.Drawing.Size(111, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nuevo Respaldo";
+            // 
+            // lblDirectorio
+            // 
+            this.lblDirectorio.AutoSize = true;
+            this.lblDirectorio.Location = new System.Drawing.Point(67, 155);
+            this.lblDirectorio.Name = "lblDirectorio";
+            this.lblDirectorio.Size = new System.Drawing.Size(63, 13);
+            this.lblDirectorio.TabIndex = 4;
+            this.lblDirectorio.Text = "Directorio";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(199, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(326, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(545, 152);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 22);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
             // 
             // generar_respaldo
             // 
@@ -118,5 +150,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtNombreRespaldo;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDirectorio;
     }
 }
